088a33560538c6a01103e1e4a0730b38b9e84d2d
anaBelenke <ana.cbs008@gmail.com>
2022-10-04 19:58:29 -0300

    Função de filtro adicionado ao Portifolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,11 +52,52 @@ var swiper = new Swiper(".testimonial-wrapper", {
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
 /*=============== PORTFOLIO ITEM FILTER ===============*/
+const filterContainer = document.querySelector(".portfolio-filter-inner"),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length,
+    portfolioItems = document.querySelectorAll(".portfolio-item"),
+    totalPortfolioItem = portfolioItems.length;
+
+    for(let i=0; i<totalFilterBtn; i++)
+    {
+        filterBtns[i].addEventListener('click', function()
+        {
+            filterContainer.querySelector(".active").classList.remove("active");
+            this.classList.add("active");
+
+            const filterValue = this.getAttribute("data-filter")
+            for(let k=0; k<totalPortfolioItem; k++)
+            {
+                if(filterValue === portfolioItems[k].getAttribute("data-category"))
+                {   
+                    portfolioItems[k].classList.remove("hide");
+                    portfolioItems[k].classList.add("show");
+                }
+                else 
+                {
+                    portfolioItems[k].classList.add("hide");
+                    portfolioItems[k].classList.remove("show");
+                }
+                if(filterValue === "all")
+                {
+                    portfolioItems[k].classList.remove("hide");
+                    portfolioItems[k].classList.add("show");
+                }
+
+            }
+        })
+    }
 
 /*=============== THEME/DISPLAY CUSTOMIZATION ===============*/
 const theme = document.querySelector("#theme-button");
 const themeModal = document.querySelector(".customize-theme");
-const fontSizes = document.querySelectorAll('.choose-size span')
+const fontSizes = document.querySelectorAll(".choose-size span");
+const colorPalette = document.querySelectorAll(".choose-color span");
+var root = document.querySelector(":root");
+const Bg1 = document.querySelector(".bg-1");
+const Bg2 = document.querySelector(".bg-2");
+const Bg3 = document.querySelector(".bg-3");
+
 
 //open modal 
 const openThemeModal = () => {
@@ -74,6 +115,7 @@ theme.addEventListener("click", openThemeModal);
 themeModal.addEventListener("click", closeThemeModal);
 
 /*===== FONTS =====*/
+//remove active class from spans pr font size selectors
 const removeSizeSelector = () => {
     fontSizes.forEach(size => {
         size.classList.remove("active");
@@ -106,6 +148,87 @@ fontSizes.forEach(size => {
         document.querySelector('html').style.fontSize = fontSize;
     })
 })
+
 /*===== PRIMARY COLORS =====*/
+//remove active class from colors 
+const changeActiveColorClass = () => {
+    colorPalette.forEach(colorPicker => {
+        colorPicker.classList.remove('active');
+    })
+}
+colorPalette.forEach(color => {
+    color.addEventListener('click', () => {
+
+        let primaryHue; 
+        changeActiveColorClass();
+
+        if(color.classList.contains('color-1'))
+        {
+            primaryHue = 252; 
+        }
+        else if(color.classList.contains('color-2'))
+        {
+            primaryHue = 52; 
+        }
+        else if(color.classList.contains('color-3'))
+        {
+            primaryHue = 352; 
+        }
+        else if(color.classList.contains('color-4'))
+        {
+            primaryHue = 152; 
+        }
+        else if(color.classList.contains('color-5'))
+        {
+            primaryHue = 202; 
+        }
+        color.classList.add("active");
+        root.style.setProperty('--primary-color-hue', primaryHue);
+    })
+})
 
 /*===== THEME BACKGROUNDS =====*/
+let lightColorLightness; 
+let whiteColorLightness; 
+let darkColorLightness; 
+
+// change background color 
+const changeBG = () => {
+    root.style.setProperty('--light-color-lightness', lightColorLightness);
+    root.style.setProperty('--white-color-lightness', whiteColorLightness);
+    root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+Bg1.addEventListener('click', () => {
+    //add active class 
+    Bg1.classList.add('active');
+    //remove active class from the others 
+    Bg2.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+    // remove customized changes from local storage 
+    window.location.reload();
+})
+Bg2.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '20%';
+    lightColorLightness = '15%';
+
+    //add active class 
+    Bg2.classList.add('active');
+    //remove active class from the others 
+    Bg1.classList.remove('active');
+    Bg3.classList.remove('active');
+    changeBG();
+})
+Bg3.addEventListener('click', () => {
+    darkColorLightness = '95%';
+    whiteColorLightness = '10%';
+    lightColorLightness = '0%';
+
+    //add active class 
+    Bg3.classList.add('active');
+    //remove active class from the others 
+    Bg2.classList.remove('active');
+    Bg1.classList.remove('active');
+    changeBG();
+})
